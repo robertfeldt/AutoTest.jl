7ee5b2fba9e29007d7e34d3e9c828045310c8395
Robert Feldt <robert.feldt@gmail.com>
2013-11-03 17:15:26 +0100

    print more, there are problems

--- src/utils/recurse_files.jl
+++ src/utils/recurse_files.jl
@@ -1,6 +1,8 @@
 recurse_and_find_all_files_matching(callback, dir, regexp = r".*") = begin
   for(file in readdir(dir))
+    println(file)
     filepath = join([dir, "/", file])
+    println(filepath)
     if isdir(filepath)
       recurse_and_find_all_files_matching(callback, filepath, regexp)
     elseif match(regexp, filepath) != nothing
